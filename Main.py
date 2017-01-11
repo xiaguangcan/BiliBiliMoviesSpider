f35da9f0f502ccefb1c31dee325dc2b54b04bc64
xiaguangcan <gcxia@ctrip.com>
2017-01-11 14:55:38 +0800

    完善

--- Main.py
+++ Main.py
@@ -2,15 +2,26 @@
 from BiliBiliSpider import *
 import Sqlite3Util
 
-if __name__ == '__main__':
+category_list = (
+    '剧情', '喜剧', '爱情', '美国', '恐怖', '经典', '动作', '悬疑', '科幻', '奇幻', '冒险', '战争', '神作', '文艺', '搞笑', '法国', '青春', '英国', '治愈向',
+    '萝莉')
 
+
+def get_different_category_movies(max_page_num, category):
     bili_spider = BiliBiliSpider()
-    movies_list = bili_spider.driver_bilibili('动作')
-    Sqlite3Util.create_bili_table()
-    for item in movies_list:
-        if Sqlite3Util.check_movie_exist(item.bili_movies_url):
-            Sqlite3Util.update_bili_movies(item)
-        else:
-            Sqlite3Util.insert_bili_movies(item)
 
+    while bili_spider.page <= max_page_num:
+        movies_list = bili_spider.driver_bilibili(category)
+        Sqlite3Util.create_bili_table()
+        for item in movies_list:
+            if Sqlite3Util.check_movie_exist(item.bili_movies_url):
+                Sqlite3Util.update_bili_movies(item)
+            else:
+                Sqlite3Util.insert_bili_movies(item)
+        bili_spider.page += 1
+
+
+if __name__ == '__main__':
+    global category_list
+    get_different_category_movies(5, category_list[0])
     Sqlite3Util.close_db()
--- README.md
+++ README.md
@@ -3,3 +3,8 @@ B站电影爬虫
 爬取B站的电影：<br>  包括电影名称，图片，电影地址，描述，观看数
 
 保存本地数据库
+
+##使用
+ 替换Main.py中的category还可以设置抓取的页面的个数
+
+ get_different_category_movies(5, category_list[0])
